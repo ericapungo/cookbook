8cd2da2e1a3e110194a9d7661dedf0f0f2a40d79
JCaila <jcaila84@gmail.com>
2013-06-15 20:56:20 +0100

    Revert "Revert "Revert "Revert "Merge branch 'develop' of https://github.com/ericapungo/cookbook into develop""""
    This reverts commit 2a0408e91cb037670e93a4db05e7db8080c64784.

--- src/main/java/pt/ulht/es/cookbook/domain/Recipe.java
+++ src/main/java/pt/ulht/es/cookbook/domain/Recipe.java
@@ -1,5 +1,6 @@
 package pt.ulht.es.cookbook.domain;
 
+<<<<<<< HEAD
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
@@ -67,4 +68,83 @@ public class Recipe extends Recipe_Base implements Comparable<Recipe> {
 
 
     
+=======
+public class Recipe {
+
+	private String id;
+	private String titulo;
+	private String problema;
+	private String solucao;
+	public String autor;
+	public String data;
+	public String hora;
+
+	public Recipe(String titulo, String problema, String solucao, String autor,
+			String data, String hora) {
+		this.titulo = titulo;
+		this.problema = problema;
+		this.solucao = solucao;
+		this.autor = autor;
+		this.data = data;
+		this.hora = hora;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getProblema() {
+		return problema;
+	}
+
+	public void setProblema(String problema) {
+		this.problema = problema;
+	}
+
+	public String getSolucao() {
+		return solucao;
+	}
+
+	public void setSolucao(String solucao) {
+		this.solucao = solucao;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+
+	}
+
+>>>>>>> b99a387add36f530669d18f896921a267ec95869
 }
