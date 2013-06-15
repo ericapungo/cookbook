191fad491bb70cb39006834f217266a71d9d8fb6
JCaila <jcaila84@gmail.com>
2013-06-15 20:56:25 +0100

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Merge branch 'develop' of https://github.com/ericapungo/cookbook into develop""""""""
    This reverts commit f85fcd3c99284fe68f1685329d14448eb20203a2.

--- src/main/java/pt/ulht/es/cookbook/domain/CookbookManager.java
+++ src/main/java/pt/ulht/es/cookbook/domain/CookbookManager.java
@@ -1,5 +1,6 @@
 package pt.ulht.es.cookbook.domain;
 
+<<<<<<< HEAD
 import pt.ist.fenixframework.FenixFramework;
 
 public class CookbookManager extends CookbookManager_Base {
@@ -8,4 +9,45 @@ public class CookbookManager extends CookbookManager_Base {
 		return FenixFramework.getRoot();
 	}
     
+=======
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CookbookManager {
+
+	private static int nextRecipeId = 1;
+	private static Map<String, Recipe> RECIPE_MAP = new HashMap<String, Recipe>();
+
+	public static void saveRecipe(Recipe recipe) {
+		String id = nextRecipeId + "";
+		RECIPE_MAP.put(id, recipe);
+		recipe.setId(id);
+		nextRecipeId++;
+
+	}
+
+	public static Recipe getRecipe(String id) {
+		return RECIPE_MAP.get(id);
+	}
+
+	public static Collection<Recipe> getRecipes() {
+		return RECIPE_MAP.values();
+
+	}
+
+	public static Collection<Recipe> removeRecipe(String id) {
+
+		RECIPE_MAP.remove(id);
+		return RECIPE_MAP.values();
+	}
+
+	public static Collection<Recipe> editRecipe (Recipe recipe) {
+	
+		RECIPE_MAP.put("2", recipe);
+		return RECIPE_MAP.values();
+	
+	}
+
+>>>>>>> b99a387add36f530669d18f896921a267ec95869
 }
